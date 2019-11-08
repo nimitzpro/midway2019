a72dcf3b77ea69b9b5aefa652f8824d62ea88f88
nimitz <astroalex5@gmail.com>
2019-11-08 23:22:52 +0000

    First addition

---
+++ battle.js
@@ -0,0 +1,44 @@
+let left1 = document.getElementById("leftimg");
+let left2 = document.getElementById("leftimg2");
+function left(){
+    left1.style.display = "none";
+    left2.style.display = "inline-block";
+}
+function leftout(){
+    left1.style.display = "inline-block";
+    left2.style.display = "none";
+}
+
+let right1 = document.getElementById("rightimg");
+let right2 = document.getElementById("rightimg2");
+function right(){
+    right1.style.display = "none";
+    right2.style.display = "inline-block";
+}
+function rightout(){
+    right1.style.display = "inline-block";
+    right2.style.display = "none";
+}
+
+var i = 1;
+var j = 1;
+var y = "i";
+window.onload = () => {
+    document.getElementById(y+i.toString()).style.display = "block";
+    document.getElementById("page").innerHTML = "Page " + i.toString() + " out of 2";
+};
+
+function changeRight(){
+    j = i;
+    i++;
+    document.getElementById(y+i.toString()).style.display = "block";
+    document.getElementById(y+j.toString()).style.display = "none";
+    document.getElementById("page").innerHTML = "Page " + i.toString() + " out of 2";
+}
+function changeLeft(){
+    j = i;
+    i--;
+    document.getElementById(y+i.toString()).style.display = "block";
+    document.getElementById(y+j.toString()).style.display = "none";
+    document.getElementById("page").innerHTML = "Page " + i.toString() + " out of 2";
+}
