909ed953fb2bc996ab43449c212d8cfa46987fb9
nimitzpro <astroalex5@gmail.com>
2019-11-11 09:51:38 +0000

    Wikipedia

--- battle.js
+++ battle.js
@@ -1,41 +1,41 @@
-let left1 = document.getElementById("leftimg");
-let left2 = document.getElementById("leftimg2");
-function left(){
-    left1.style.display = "none";
-    left2.style.display = "inline-block";
-}
-function leftout(){
-    left1.style.display = "inline-block";
-    left2.style.display = "none";
-}
+// let left1 = document.getElementById("leftimg");
+// let left2 = document.getElementById("leftimg2");
+// function left(){
+//     left1.style.display = "none";
+//     left2.style.display = "inline-block";
+// }
+// function leftout(){
+//     left1.style.display = "inline-block";
+//     left2.style.display = "none";
+// }
 
-let right1 = document.getElementById("rightimg");
-let right2 = document.getElementById("rightimg2");
-function right(){
-    right1.style.display = "none";
-    right2.style.display = "inline-block";
-}
-function rightout(){
-    right1.style.display = "inline-block";
-    right2.style.display = "none";
-}
+// let right1 = document.getElementById("rightimg");
+// let right2 = document.getElementById("rightimg2");
+// function right(){
+//     right1.style.display = "none";
+//     right2.style.display = "inline-block";
+// }
+// function rightout(){
+//     right1.style.display = "inline-block";
+//     right2.style.display = "none";
+// }
 
 var i = 1;
 var j = 1;
 var y = "i";
 window.onload = () => {
     document.getElementById(y+i.toString()).style.display = "block";
-    document.getElementById("page").innerHTML = "Page " + i.toString() + " out of 2";
+    document.getElementById("page").innerHTML = "Page " + i.toString() + " out of 8";
 };
 
 function changeRight(){
-    if(i != 2){
+    if(i != 8){
         j = i;
         i++;
     }
     document.getElementById(y+i.toString()).style.display = "block";
     document.getElementById(y+j.toString()).style.display = "none";
-    document.getElementById("page").innerHTML = "Page " + i.toString() + " out of 2";
+    document.getElementById("page").innerHTML = "Page " + i.toString() + " out of 8";
 }
 function changeLeft(){
     if(i != 1){
@@ -44,5 +44,5 @@ function changeLeft(){
     }
     document.getElementById(y+i.toString()).style.display = "block";
     document.getElementById(y+j.toString()).style.display = "none";
-    document.getElementById("page").innerHTML = "Page " + i.toString() + " out of 2";
+    document.getElementById("page").innerHTML = "Page " + i.toString() + " out of 8";
 }
